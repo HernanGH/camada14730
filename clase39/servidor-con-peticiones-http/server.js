a07b0540ccdfc19c2a85c63039cc5c1252848d72
Hernan Condori <hcondori@elementum.com>
2021-12-02 22:31:02 -0300

    - fix identation

--- clase39/servidor-con-peticiones-http/server.js
+++ clase39/servidor-con-peticiones-http/server.js
@@ -1,5 +1,7 @@
-require('http').createServer((req,res) => {
+require('http')
+.createServer((req,res) => {
     res.writeHead(200, {'content-type':'application/json'})
-    res.end(JSON.stringify({FyH: new Date().toLocaleString()}))
-}).listen(8080, () => console.log(`Servidor escuchando en el puerto 8080`))
+    res.end(JSON.stringify({ FyH: new Date().toLocaleString() }))
+})
+.listen(8080, () => console.log(`Servidor escuchando en el puerto 8080`))
 
